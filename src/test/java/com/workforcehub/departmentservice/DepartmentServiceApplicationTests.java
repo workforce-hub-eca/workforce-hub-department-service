8972831c7e6b03974824197e8e402538d69db06e
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-08-23 00:29:34 +0530

    chore(deploy): prepare Department Service for GCP

--- src/test/java/com/workforcehub/departmentservice/DepartmentServiceApplicationTests.java
+++ src/test/java/com/workforcehub/departmentservice/DepartmentServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.workforcehub.departmentservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(args = "--spring.config.name=application-test")
 class DepartmentServiceApplicationTests {
 
 	@Test
